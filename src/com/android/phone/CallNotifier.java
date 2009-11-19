37c804c316a280f0e7d48a2973667bc471b544b5
Jaime Lopez <c_jaimel@quicinc.com>
2009-11-19 09:20:47 -0800

    apps/Phone: Log / don't log emergency calls in CDMA
    	Added a condition to CallNotifier.java to check the value
            of property ro.cdma.log_emergency_calls and whether
    	the call is an emergency call to decide if it should be
    	logged.
    
    	CRs-Fixed: 219302

--- src/com/android/phone/CallNotifier.java
+++ src/com/android/phone/CallNotifier.java
@@ -887,6 +887,10 @@ public class CallNotifier extends Handler
             }
             if (VDBG) log("- callLogType: " + callLogType + ", UserData: " + c.getUserData());
 
+            // Log emergency calls depending on the value of the property ro.cdma.log_emergency_calls
+            if(!mPhone.getPhoneName().equals("CDMA") ||
+                    !PhoneNumberUtils.isEmergencyNumber(number) ||
+                    SystemProperties.getBoolean("ro.cdma.log_emergency_calls", false))
             // Get the CallerInfo object and then log the call with it.
             {
                 Object o = c.getUserData();
