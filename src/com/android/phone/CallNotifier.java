a5bf5d986f4eddcd5da81e3c1e73993a1cb215ae
Pawan Kumar Nimmagadda <pawan@qualcomm.com>
2010-03-17 00:15:39 +0530

    Ringback tone is not stopped when Phonecall is disconnected locally in DIALING or ALERTING  state
    The ringback tone is stopped when phone moves out of DIALING or ALERTING
    state. Incases where Phone gets disconnected locally in DIALING or ALERTING
    state the ringbacktone is not stopped. Hence stopping it in onDestroy.
    
    Change-Id: I9a1673716a32d55f8142d00c37e9a8bed2ac062d
    CRs-Fixed:230392

--- src/com/android/phone/CallNotifier.java
+++ src/com/android/phone/CallNotifier.java
@@ -640,6 +640,7 @@ public class CallNotifier extends Handler
                 // tone stops, we have to stop it to prevent disturbing.
                 if (mInCallRingbackTonePlayer != null &&
                         mCurrentRingbackToneState == RINGBACK_TONE_ON) {
+                    if (DBG) log("stop RingbacktonePlayer in onPhoneStateChanged()");
                     mInCallRingbackTonePlayer.stop();
                 }
             }
@@ -931,6 +932,21 @@ public class CallNotifier extends Handler
             // Remove Call waiting timers
             removeMessages(CALLWAITING_CALLERINFO_DISPLAY_DONE);
             removeMessages(CALLWAITING_ADDCALL_DISABLE_TIMEOUT);
+        }
+        /** In cases where local ringback tone is played and phone gets disconnected
+         ** locally/at remote end in DIALING or ALERTING state, the ringtone is not stopped.
+         ** In Such cases we stop the ring back tone here.  */
+        if (mPhone.getPhoneName().equals("GSM")) {
+            Call.State callState = mPhone.getForegroundCall().getState();
+            if (!callState.isDialing()) {
+                // If call get disconnected before the ringback
+                // tone stops, we have to stop it to prevent disturbing.
+                if (mInCallRingbackTonePlayer != null &&
+                        mCurrentRingbackToneState == RINGBACK_TONE_ON) {
+                    if (DBG) log("stop RingbacktonePlayer in onDisconnect");
+                    mInCallRingbackTonePlayer.stop();
+                }
+            }
         }
     }
 
@@ -1623,11 +1639,13 @@ public class CallNotifier extends Handler
             if (mInCallRingbackTonePlayer != null
                     && mPhone.getForegroundCall().getState().isDialing()
                     && mCurrentRingbackToneState == RINGBACK_TONE_OFF) {
+                if (DBG) log("Start local ringbacktone");
                 mInCallRingbackTonePlayer.start();
             }
         } else {
             if (mInCallRingbackTonePlayer != null &&
                     mCurrentRingbackToneState == RINGBACK_TONE_ON) {
+                if (DBG) log("Stop local ringbacktone");
                 mInCallRingbackTonePlayer.stop();
             }
         }
