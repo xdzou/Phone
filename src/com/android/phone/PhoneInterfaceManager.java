9de3f3903a7e53e7d303c57863f187c5008b409e
Susheel Yadagiri <syadagir@codeaurora.org>
2010-08-16 21:34:12 -0700

    apps/Phone: Add apis to get gatewayAddr,ifaceName,ipAddr specific to ipversion.
    Change-Id: Ia046f892374b48cd04a9cae9b1d7b7381cbed2da

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -38,6 +38,7 @@ import com.android.internal.telephony.ITelephony;
 import com.android.internal.telephony.Phone;
 import com.android.internal.telephony.TelephonyIntents;
 import com.android.internal.telephony.CommandException;
+import com.android.internal.telephony.DataPhone.IPVersion;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -813,6 +814,14 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
      * {@hide}
      * Returns Interface name
      */
+    public String getActiveInterfaceName(String apnType, String ipv) {
+        return mPhone.getInterfaceName(apnType,Enum.valueOf(IPVersion.class, ipv));
+    }
+
+    /**
+     * {@hide}
+     * Returns Interface name
+     */
     public String getActiveInterfaceName(String apnType) {
         return mPhone.getInterfaceName(apnType);
     }
@@ -822,8 +831,24 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
      * {@hide}
      * Returns Ip address
      */
+    public String getActiveIpAddress(String apnType, String ipv) {
+        return mPhone.getIpAddress(apnType,Enum.valueOf(IPVersion.class, ipv));
+    }
+
+    /**
+     * {@hide}
+     * Returns Ip address
+     */
     public String getActiveIpAddress(String apnType) {
         return mPhone.getIpAddress(apnType);
+    }
+
+    /**
+     * {@hide}
+     * Returns Gateway address
+     */
+    public String getActiveGateway(String apnType, String ipv) {
+        return mPhone.getGateway(apnType,Enum.valueOf(IPVersion.class, ipv));
     }
 
     /**
