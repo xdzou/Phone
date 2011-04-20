d38cefb59424a7c6bf377def25df18bbca92974a
Kalyani polepeddy <kpolep@codeaurora.org>
2011-04-20 14:54:36 +0530

    Phone : When call is on hold,get phone in call instead of default phone.
    When RAT changes occur,phones get registered and de-registered with CallManager and
    default phone of CallManager changes accordingly. When a call is on hold,getActiveFgCall()
    returns default phone,which is not the actual phone in use. So,getPhoneInCall() is used
    instead.
    
    CRs-Fixed: 283808
    (cherry picked from commit 53cf16601ac9dd65a3c0f5bb8fa4a348befee4f2)
    
    Change-Id: I65edf63a7a3f7541b589ed07679bdc6eea2489d4

--- src/com/android/phone/InCallControlState.java
+++ src/com/android/phone/InCallControlState.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009 The Android Open Source Project
+ * Copyright (C) 2009,2011 The Android Open Source Project
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -159,7 +159,7 @@ public class InCallControlState {
         dialpadVisible = mInCallScreen.isDialerOpened();
 
         // "Hold:
-        if (TelephonyCapabilities.supportsHoldAndUnhold(fgCall.getPhone())) {
+        if (TelephonyCapabilities.supportsHoldAndUnhold(mCM.getPhoneInCall())) {
             // This phone has the concept of explicit "Hold" and "Unhold" actions.
             supportsHold = true;
             // "On hold" means that there's a holding call and
