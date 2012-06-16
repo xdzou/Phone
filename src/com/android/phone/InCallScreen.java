829b0aa98d05a80f6c8e2da21a57b4baf0ab8e3d
Hariprasad Jayakumar <hjayak@codeaurora.org>
2012-06-15 17:51:36 -0700

    Phone: Remove stale DELAYED_CLEANUP_AFTER_DISCONNECT
    During phone switch due to RAT change, any pending
    DELAYED_CLEANUP_AFTER_DISCONNECT message in InCallScreen
    should be dropped. If they are not dropped they will be
    accessing destroyed phone objects even after phone switch.
    
    Change-Id: Iec801f4150fbbcd36aaa3e207eb116c9597ba2c2
    CRs-Fixed: 370584

--- src/com/android/phone/InCallScreen.java
+++ src/com/android/phone/InCallScreen.java
@@ -1130,6 +1130,8 @@ public class InCallScreen extends Activity
         mCM.unregisterForSuppServiceFailed(mHandler);
         mCM.unregisterForIncomingRing(mHandler);
         mCM.unregisterForNewRingingConnection(mHandler);
+        // remove locally posted message
+        mHandler.removeMessages(DELAYED_CLEANUP_AFTER_DISCONNECT);
         Phone phone = PhoneUtils.getImsPhone(mCM);
         try {
             if (phone != null) {
