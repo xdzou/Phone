c3de8af62f7039c4b5db2a871207cc684935c280
Suresh Koleti <skolet@codeaurora.org>
2012-09-07 16:26:01 +0530

    Phone(DSDS): Update preferred networkmode values properly in database.
    Preferred networkmode values to be updated per subscription.
    
    Change-Id: I60834d87361c8560cbe949c1fa71434a22d6ea9b

--- src/com/android/phone/Use2GOnlyCheckBoxPreference.java
+++ src/com/android/phone/Use2GOnlyCheckBoxPreference.java
@@ -54,8 +54,7 @@ public class Use2GOnlyCheckBoxPreference extends CheckBoxPreference {
 
         int networkType = isChecked() ? Phone.NT_MODE_GSM_ONLY : Phone.NT_MODE_WCDMA_PREF;
         Log.i(LOG_TAG, "set preferred network type="+networkType);
-        android.provider.Settings.Secure.putInt(mPhone.getContext().getContentResolver(),
-                android.provider.Settings.Secure.PREFERRED_NETWORK_MODE, networkType);
+        setPreferredNetworkMode(networkType);
         mPhone.setPreferredNetworkType(networkType, mHandler
                 .obtainMessage(MyHandler.MESSAGE_SET_PREFERRED_NETWORK_TYPE));
    }
@@ -65,6 +64,13 @@ public class Use2GOnlyCheckBoxPreference extends CheckBoxPreference {
         mPhone = phone;
         mPhone.getPreferredNetworkType(
                  mHandler.obtainMessage(MyHandler.MESSAGE_GET_PREFERRED_NETWORK_TYPE));
+    }
+
+    private void setPreferredNetworkMode(int nwMode) {
+        android.provider.Settings.Secure.putIntAtIndex(
+                    mPhone.getContext().getContentResolver(),
+                    android.provider.Settings.Secure.PREFERRED_NETWORK_MODE,
+                    mPhone.getSubscription(), nwMode);
     }
 
     private class MyHandler extends Handler {
@@ -92,8 +98,7 @@ public class Use2GOnlyCheckBoxPreference extends CheckBoxPreference {
                 int type = ((int[])ar.result)[0];
                 Log.i(LOG_TAG, "get preferred network type="+type);
                 setChecked(type == Phone.NT_MODE_GSM_ONLY);
-                android.provider.Settings.Secure.putInt(mPhone.getContext().getContentResolver(),
-                        android.provider.Settings.Secure.PREFERRED_NETWORK_MODE, type);
+                setPreferredNetworkMode(type);
             } else {
                 // Weird state, disable the setting
                 Log.i(LOG_TAG, "get preferred network type, exception="+ar.exception);
