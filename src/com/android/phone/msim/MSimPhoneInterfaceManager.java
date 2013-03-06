ef23847d52b76ff56ce59332dcb42011ba98367e
tianhaiyan <tianhaiyan@hisensecom.com>
2013-03-06 14:41:07 +0800

    merge from 8x25q(8X30pre_FC): add function getVoiceRadioTechnology(),getNetworkName(),getCellLocation()

--- src/com/android/phone/msim/MSimPhoneInterfaceManager.java
+++ src/com/android/phone/msim/MSimPhoneInterfaceManager.java
@@ -49,6 +49,9 @@ import com.android.internal.telephony.PhoneConstants;
 import com.android.internal.telephony.CommandException;
 import com.android.internal.telephony.msim.ITelephonyMSim;
 import com.qualcomm.internal.telephony.SubscriptionManager;
+import android.provider.SpnProvider;
+import android.telephony.cdma.CdmaCellLocation;
+import android.telephony.TelephonyManager;
 
 import java.util.List;
 import java.util.ArrayList;
@@ -949,4 +952,48 @@ public class MSimPhoneInterfaceManager extends ITelephonyMSim.Stub {
     public int getLteOnCdmaMode(int subscription) {
         return getPhone(subscription).getLteOnCdmaMode();
     }
+
+    public String getNetworkName(int subscription) {
+        String networkName = null;
+        ServiceState serviceState = getPhone(subscription).getServiceState();
+        if (serviceState != null && serviceState.getState() == ServiceState.STATE_IN_SERVICE) {
+            String operatorNumeric = serviceState.getOperatorNumeric();
+            boolean isNumericValid = operatorNumeric != null && operatorNumeric.length() > 3
+                                     && !("00000".equals(operatorNumeric));
+            //init network name to "unknown network"
+            networkName = (String)mApp.getResources().getString(R.string.unknown_network);
+
+            Log.d(LOG_TAG, " operatorNumeric: " + operatorNumeric);
+
+            if (isNumericValid) {
+                networkName = (String)SpnProvider.getSPNByMCCMNC(mApp, operatorNumeric);
+                //if cannot get name by mcc,mnc, use mcc+mnc
+                if (TextUtils.isEmpty(networkName))
+                    return operatorNumeric;
+            } else {
+                int phoneType = getActivePhoneType(subscription);
+                if (phoneType == PhoneConstants.PHONE_TYPE_CDMA) {
+                    //if mcc+mnc is invalid, for cdma, get system network id.
+                    CdmaCellLocation cdmaCellLocation = (CdmaCellLocation)MSimTelephonyManager.getDefault()
+                                                        .getCellLocation(subscription);
+                    if (cdmaCellLocation != null) {
+                        int sid = cdmaCellLocation.getSystemId();
+                        int nid = cdmaCellLocation.getNetworkId();
+                        //if sid is valid, show sid
+                        if (sid > 0 && nid > 0) {
+                           networkName = Integer.toString(sid) + "," + Integer.toString(nid);
+                        } else if (sid > 0) {
+                           networkName = Integer.toString(sid);
+                        } else if (nid > 0) {
+                           networkName = Integer.toString(nid);
+                        }
+                    }
+                }
+            }
+        }
+
+        return networkName;
+    }
+
+
 }
