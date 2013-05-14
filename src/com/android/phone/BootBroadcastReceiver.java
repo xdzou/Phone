2ac4a8259605e7c9b8de8420fe274d9439a1f8ee
liuwei <liuwei@hisensecom.com>
2013-05-14 19:21:55 +0800

    disble callfeaturessetting in single mode

---
+++ src/com/android/phone/BootBroadcastReceiver.java
@@ -0,0 +1,37 @@
+package com.android.phone;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.telephony.TelephonyManager;
+import android.content.pm.PackageManager;
+import android.content.ComponentName;
+import android.util.Log;
+
+public class BootBroadcastReceiver extends BroadcastReceiver {
+
+    private static final String TAG = "BootBroadcastReceiver";
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        String action = intent.getAction();
+        Log.d(TAG, "receive action: " + action);
+        if (Intent.ACTION_BOOT_COMPLETED.equals(action)) {
+            if (!TelephonyManager.isMultiSimEnabled())
+            {
+               Log.d(TAG, "disable MSimCallFeaturesSubSetting");
+               PackageManager pm = context.getPackageManager();
+               pm.setComponentEnabledSetting(new ComponentName("com.android.phone",
+                                             "com.android.phone.MSimCallFeaturesSubSetting"),
+                                             PackageManager.COMPONENT_ENABLED_STATE_DISABLED, 0);
+            }else{
+                Log.d(TAG, "disable CallFeaturesSetting");
+                PackageManager pm = context.getPackageManager();
+                pm.setComponentEnabledSetting(new ComponentName("com.android.phone",
+                                              "com.android.phone.CallFeaturesSetting"),
+                                              PackageManager.COMPONENT_ENABLED_STATE_DISABLED, 0);
+            }
+            return;
+        } 
+    }
+}
